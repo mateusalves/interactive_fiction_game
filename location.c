7cace73a5743d5b411e2ffa8bbf9e21ea247dd17
Mateus Alves <mateusonboard@gmail.com>
2020-12-22 18:34:29 -0300

    Adding the new functions to location.c

--- location.c
+++ location.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
-struct location {
-    const char *description;
-    const char *tag;
-}
-locs[] = {
-    {"an open field", "field"},
-    {"a little cave", "cave"},
-    {"a boat at the lake", "lake"},             //I added this
-    {"a trail at the mountain", "mountain"}     //I added this
-};
-
-#define numberOfLocations (sizeof locs/ sizeof *locs)
-
-static unsigned locationOfPlayer = 0;
+#include "object.h"
+#include "misc.h"
+#include "noun.h"
 
 void executeLook(const char *noun)
 {
     if(noun != NULL && strcmp(noun, "around") == 0)
     {
-        printf("You are in %s.\n", locs[locationOfPlayer].description);
+        printf("You are in %s.\n", player->location->description);
+        listObjectsAtLocation(player->location);
     }
     else
     {
@@ -31,23 +20,19 @@ void executeLook(const char *noun)
 
 void executeGo (const char *noun)
 {
-    unsigned i;
-    for (i = 0; i < numberOfLocations; i++)
+    OBJECT *obj = getVisible("where you want to go", noun);
+    if (obj == NULL)
     {
-        if (noun != NULL && strcmp(noun, locs[i].tag) == 0)
-        {
-            if (i == locationOfPlayer)
-            {
-                printf("You can't get much closer than this.\n");
-            }
-            else
-            {
-                printf("Ok.\n");
-                locationOfPlayer = i;
-                executeLook("around");
-            }
-            return;
-        }
+        //already handled by getVisible
     }
-    printf("I don't understand where you want to go.\n");
+    else if (obj->location == NULL && obj != player->location)
+    {
+        printf("Ok.\n");
+        player->location = obj;
+        executeLook("around");
+    }
+    else
+    {
+        printf("You can't get much closer than this.\n");
+    }
 }
